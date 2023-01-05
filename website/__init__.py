65fb0bb4af283f844c3fafb1d061f437b5e7afe1
Matth-M <matthieu.matsima@gmail.com>
2023-01-06 00:15:15 +0100

    Setup database

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,26 @@
-import os
-
+from os import path
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dev'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Reservation
+
+    with app.app_context():
+        db.create_all()
 
     return app
