8daac85d48d4d917558ba76c54bcba72f8daf024
Matth-M <matthieu.matsima@gmail.com>
2023-01-06 14:34:17 +0100

    Create database

---
+++ website/schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE user (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	email TEXT UNIQUE NOT NULL,
+	username TEXT UNIQUE NOT NULL,
+	password TEXT NOT NULL
+);
+
+-- Reserveation of a slot in the gym calendar
+CREATE TABLE reservation (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_id INTEGER NOT NULL,
+	time TIMESTAMP NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES user (id)
+);
