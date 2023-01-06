c217006c62e34bbc6f0dd9c71b5044deadcbaa49
Matth-M <matthieu.matsima@gmail.com>
2023-01-06 14:32:23 +0100

    Add templates and views for registration

--- website/__init__.py
+++ website/__init__.py
@@ -1,26 +1,52 @@
-from os import path
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
+# from flask_sqlalchemy import SQLAlchemy
+#
+# db = SQLAlchemy()
+# DB_NAME = "database.db"
 
 
 def create_app(test_config=None):
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'dev'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Import blueprints
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views)
+    app.register_blueprint(auth)
 
-    from .models import User, Reservation
+    from . import db
+    db.init_app(app)
 
-    with app.app_context():
-        db.create_all()
+    # app = Flask(__name__)
+    # app.config['SECRET_KEY'] = 'dev'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # db.init_app(app)
+    #
+    #
+    # from .models import User, Reservation
+    #
+    # with app.app_context():
+    #     db.create_all()
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,38 @@
-from flask import Blueprint, redirect, render_template, url_for, request, flash
+import functools
 
-auth = Blueprint('auth', __name__)
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from website.db import get_db
+
+auth = Blueprint('auth', __name__, url_prefix='/auth')
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        error = None
+        db = get_db()
+        user = db.execute(
+                "select * from user where email = ?", (email,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect email.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect Password'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            redirect('views.home')
+
+
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -14,19 +43,54 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstname = request.form.get('firstname')
-        lastname = request.form.get('lastname')
+        username = request.form.get('username')
         password = request.form.get('password')
         password_confirm = request.form.get('password-confirm')
 
-        if len(email) < 5:
-            flash('Email is too short.', category='error')
-        elif password != password_confirm:
-            flash('Passwords are not matching.', category='error')
-        elif len(firstname) > 50:
-            flash('Firstname is too long.', category='error')
-        else:
-            flash('Account created successfully!', category='success')
+        db = get_db()
+        error = None
 
-        # access database
+        if not username:
+            error = 'Username required.'
+        elif not password:
+            error = 'Password required'
+        elif not email:
+            error = 'Email required'
+        elif password != password_confirm:
+            error = "Passwords don't match"
+
+        if error is None:
+            try:
+                db.execute(
+                        "INSERT INTO user (email, username, password) VALUES (?, ?, ?)",
+                        (email, username, generate_password_hash(password))
+                )
+
+            except db.IntegrityError:
+                error = f'{email} is already email.'
+            else: 
+                redirect('auth.login')
+
     return render_template("sign_up.html")
+
+@auth.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+                "SELECT * FROM user WHERE id = ?", (user_id,)
+        ).fetchone()
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+
+    return wrapped_view
+
