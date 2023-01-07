bd6e08d9a0dc6d6e05f46d8265b2c814bc8a893d
Matth-M <matthieu.matsima@gmail.com>
2023-01-08 00:13:51 +0100

    Add gym blueprint and its planning view

--- website/views.py
+++ website/views.py
@@ -1,20 +1,8 @@
 from flask import Blueprint, render_template
 
-from website.db import get_db
-
-views = Blueprint('views', __name__, url_prefix='/')
+views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def index():
     return render_template("index.html")
-
-@views.route('/calendar')
-def calendar():
-    # db = get_db()
-    # reservations = db.execute(
-    #         'SELECT r.id, user_id, time, username'
-    #         ' FROM reservation r JOIN user u ON p.user_id = u.id'
-    # ).fetchall()
-
-    return render_template('calendar.html')
