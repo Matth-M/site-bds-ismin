7ab75725b4c7ecc2d20751498391d5e6c6dff851
Matth-M <matthieu.matsima@gmail.com>
2023-01-08 01:17:02 +0100

    Add input in gym's planning

--- website/gym.py
+++ website/gym.py
@@ -1,15 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from website.db import get_db
 
 gym = Blueprint('gym', __name__, url_prefix='/gym')
 
-@gym.route('/planning')
+@gym.route('/planning', methods=['GET', 'POST'])
 def planning():
     db = get_db()
     reservations = db.execute(
             'SELECT r.id, user_id, time, username'
             ' FROM reservation r JOIN user u ON r.user_id = u.id'
     ).fetchall()
+
+    if request.method == 'POST':
+        date = request.form.get('time')
+        print(date)
 
     return render_template('gym_planning.html', reservations=reservations)
