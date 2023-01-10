0d76f0bc89181bb4599d5fb79e273ef971c96cec
Matth-M <matthieu.matsima@gmail.com>
2023-01-10 20:39:03 +0100

    Parse reservations and convert them to Reservation class

--- website/static/main.js
+++ website/static/main.js
@@ -1,4 +1,26 @@
-let reservationsDiv = document.querySelector('#reservations');
-let reservations = JSON.parse(reservationsDiv.dataset.reservations);
+const WEEK_LENGTH = 604800000;
 
-console.log(reservations, 'reservations');
+let reservationsDiv = document.querySelector("#reservations");
+let reservationsJSON = JSON.parse(reservationsDiv.dataset.reservations);
+
+console.log(reservationsJSON, "reservations");
+
+class Reservation {
+	constructor(id, time, user_id) {
+		this.id = id;
+		this.time = new Date(time);
+		this.user_id = user_id;
+	}
+}
+
+let reservations = [];
+reservationsJSON.forEach((reservationJSON) => {
+	let reservation = new Reservation(
+		reservationJSON.id,
+		reservationJSON.time,
+		reservationJSON.user_id
+	);
+
+	reservations.push(reservation);
+
+});
