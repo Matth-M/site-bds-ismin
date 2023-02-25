012af7604006dc23a5edfddbe813a38bad9e1dfd
Matth-M <matthieu.matsima@gmail.com>
2023-02-25 22:30:02 +0100

    Genearate planning rows dynamically
    Remove serialisation

--- website/__init__.py
+++ website/__init__.py
@@ -11,13 +11,13 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -33,11 +33,12 @@ def create_app(test_config=None):
     from .auth import auth
     from .gym import gym
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/auth')
-    app.register_blueprint(gym, url_prefix='/auth')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/auth")
+    app.register_blueprint(gym, url_prefix="/auth")
 
     from . import db
+
     db.init_app(app)
 
     return app
--- website/gym.py
+++ website/gym.py
@@ -1,5 +1,4 @@
 import datetime
-import json
 
 from flask import Blueprint
 from flask import url_for
@@ -13,47 +12,35 @@ from website.auth import login_required
 
 from website.db import get_db
 
-gym = Blueprint('gym', __name__, url_prefix='/gym')
+gym = Blueprint("gym", __name__, url_prefix="/gym")
 
-@gym.route('/planning', methods=['GET'])
+
+@gym.route("/planning", methods=["GET"])
 def planning():
     db = get_db()
     # Fetch all the reservations
-    reservations = db.cursor().execute(
-            'SELECT r.id AS ID, r.time AS time, u.id AS user_id, u.username'
-            ' FROM reservation r'
-            ' JOIN user u ON r.user_id = u.id'
-    ).fetchall()
-
-    print(reservations)
+    reservations = (
+        db.cursor()
+        .execute(
+            "SELECT r.id AS ID, r.time AS time, u.id AS user_id, u.username"
+            " FROM reservation r"
+            " JOIN user u ON r.user_id = u.id"
+        )
+        .fetchall()
+    )
 
     # Check if there are reservations made
     if reservations is None:
         reservations = []
-    else:
+    return render_template("gym_planning.html", reservations=reservations)
 
 
-        # Convert Row objects into tuples so they can be serialized
-        reservations = [{ 'id':row['id'],
-                         'time':row['time'],
-                         'user_id': row['user_id'] ,
-                         'username': row['username'],
-                         }
-                        for row in reservations]
-
-        reservations = json.dumps({'list' : reservations})
-
-
-    return render_template('gym_planning.html', reservations=reservations)
-
-
-@gym.route('/create', methods=['POST'])
+@gym.route("/create", methods=["POST"])
 @login_required
 def create():
-    if request.method == 'POST':
-
+    if request.method == "POST":
         # Get the reservation_time input by the user
-        raw_reservation_time = request.form.get('time')
+        raw_reservation_time = request.form.get("time")
         reservation_time = parse(raw_reservation_time)
 
         # Check for incorrect input
@@ -61,12 +48,12 @@ def create():
 
         # No input time
         if not reservation_time:
-            error = 'Time is required'
+            error = "Time is required"
 
         # Input time is earlier than now
         elif reservation_time.utcnow() > datetime.datetime.utcnow():
             print(reservation_time.utcnow() < datetime.datetime.utcnow())
-            error = 'Please select a valid time'
+            error = "Please select a valid time"
 
         # Insert data into the database
         if error is not None:
@@ -74,11 +61,9 @@ def create():
         else:
             db = get_db()
             db.execute(
-                "INSERT INTO reservation (time, user_id)"
-                "VALUES (?, ?)",
-                (reservation_time, g.user['id'])
+                "INSERT INTO reservation (time, user_id)" "VALUES (?, ?)",
+                (reservation_time, g.user["id"]),
             )
             db.commit()
 
-
-    return redirect(url_for('gym.planning'))
+    return redirect(url_for("gym.planning"))
