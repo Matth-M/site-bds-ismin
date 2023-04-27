88bb34ff7e6bec55ef7722be6700e2feda98417f
Matth-M <matthieu.matsima@gmail.com>
2023-04-28 01:19:39 +0200

    Create the models

---
+++ website/models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"<User: {self.email}>"
+
+
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = sa.Column(sa.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"<Reservation: {self.time}>"
