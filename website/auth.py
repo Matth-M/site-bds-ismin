c4424832516249fff0d3cde3c1d5e5b1923ea606
Matth-M <matthieu.matsima@gmail.com>
2023-04-28 04:13:12 +0200

    Format

--- website/auth.py
+++ website/auth.py
@@ -1,90 +1,98 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
 )
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from website.db import get_db
+from website.models import User
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
-@auth.route('/login', methods=['GET', 'POST'])
+
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
+    if request.method == "POST":
         # Get form data
-        email = request.form.get('email')
-        password = request.form.get('password')
+        email = request.form.get("email")
+        password = request.form.get("password")
         error = None
 
         # Fetch user
-        db = get_db()
-        user = db.execute(
-                "select * from user where email = ?", (email,)
-        ).fetchone()
+        user = db.session.scalars(select(User).where(User.email == email)).first()
 
         # Check if user exists
         if user is None:
-            error = 'Incorrect email.'
-        elif not check_password_hash(user['password'], str(password)):
-            error = 'Incorrect Password'
+            error = "Incorrect email."
+        elif not check_password_hash(user["password"], str(password)):
+            error = "Incorrect Password"
 
         # Add user info to the session
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('views.index'))
+            session["user_id"] = user["id"]
+            return redirect(url_for("views.index"))
         else:
             flash(error)
 
     return render_template("login.html")
 
-@auth.route('/logout')
+
+@auth.route("/logout")
 def logout():
     session.clear()
-    return redirect(url_for('views.index'))
+    return redirect(url_for("views.index"))
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        username = request.form.get('username')
-        password = request.form.get('password')
-        password_confirm = request.form.get('password-confirm')
+    if request.method == "POST":
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_confirm = request.form.get("password-confirm")
 
         db = get_db()
         error = None
 
         if not username:
-            error = 'Username required.'
+            error = "Username required."
         elif not password:
-            error = 'Password required'
+            error = "Password required"
         elif not email:
-            error = 'Email required'
+            error = "Email required"
         elif password != password_confirm:
             error = "Passwords don't match"
 
         if error is None:
             try:
                 db.execute(
-                        "INSERT INTO user (email, username, password) VALUES (?, ?, ?)",
-                        (email, username, generate_password_hash(password))
+                    "INSERT INTO user (email, username, password) VALUES (?, ?, ?)",
+                    (email, username, generate_password_hash(password)),
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f'{email} is already used.'
+                error = f"{email} is already used."
             else:
-                flash('User created successfully!')
-                return redirect(url_for('auth.login'))
+                flash("User created successfully!")
+                return redirect(url_for("auth.login"))
 
-        flash(f'{error}')
+        flash(f"{error}")
 
     return render_template("sign_up.html")
 
+
 @auth.before_app_request
 def load_logged_in_user():
-    user_id = session.get('user_id')
+    user_id = session.get("user_id")
 
     if user_id is None:
         g.user = None
@@ -98,7 +106,7 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('auth.login'))
+            return redirect(url_for("auth.login"))
         return view(**kwargs)
 
     return wrapped_view
