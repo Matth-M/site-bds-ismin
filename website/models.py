72637f612fa44ae7d69967db6d6181e0f90a3b29
Matth-M <matthieu.matsima@gmail.com>
2023-04-28 04:16:44 +0200

    Revert to the old API of sqlAlchemy for models
    This reverts commit f96c9e741c631a7a64a002385549ca352584fad3.

--- website/models.py
+++ website/models.py
@@ -1,41 +1,22 @@
-from datetime import datetime
-from typing import List
-from typing_extensions import Annotated
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Mapped, mapped_column
-
-from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import relationship
+import sqlalchemy as sa
 
 db = SQLAlchemy(add_models_to_shell=True)
 
-intpk = Annotated[int, mapped_column(primary_key=True)]
-str_username = Annotated[str, mapped_column(String(30))]
 
-
-class Base(DeclarativeBase):
-    pass
-
-
-class User(Base):
-    __tablename__ = "user"
-    id: Mapped[intpk]
-    email: Mapped[str] = mapped_column(String(50))
-    username: Mapped[str_username] = mapped_column(unique=True)
-    password: Mapped[str] = mapped_column(String(100))
-    reservations: Mapped[List["Reservation"]] = relationship(back_populates="user")
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return f"<User: {self.email}>"
 
 
-class Reservation(Base):
-    __tablename__ = "reservation"
-    id: Mapped[intpk]
-    time: Mapped[datetime]
-    user_username: Mapped[str_username] = mapped_column(ForeignKey("user.username"))
-    user: Mapped["User"] = relationship(back_populates="reservations")
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(sa.DateTime, nullable=False)
 
     def __repr__(self):
         return f"<Reservation: {self.time}>"
