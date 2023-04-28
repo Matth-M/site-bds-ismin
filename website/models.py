b641b631083e7e9309a90528a4d62fbdbc3485fc
Matth-M <matthieu.matsima@gmail.com>
2023-04-28 23:30:23 +0200

    Use Colum from Flask SQLAlchemy

--- website/models.py
+++ website/models.py
@@ -16,7 +16,7 @@ class User(db.Model):
 
 class Reservation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(sa.DateTime, nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
         return f"<Reservation: {self.time}>"
