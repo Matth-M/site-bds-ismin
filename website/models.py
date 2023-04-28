77882a54bffe2aa8ff638a97f1ee519fd0942891
Matth-M <matthieu.matsima@gmail.com>
2023-04-29 01:48:30 +0200

    Add a foreign key to Reservation referencing its User

--- website/models.py
+++ website/models.py
@@ -1,10 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy as sa
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy(add_models_to_shell=True)
 
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -15,8 +17,11 @@ class User(db.Model):
 
 
 class Reservation(db.Model):
+    __tablename__ = "reservation"
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey("user.id"))
+    user = relationship("User")
 
     def __repr__(self):
         return f"<Reservation: {self.time}>"
