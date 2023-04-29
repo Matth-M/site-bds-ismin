5080e6151f89848ce0aae0f4f7d9e0e319f14232
Matth-M <matthieu.matsima@gmail.com>
2023-04-29 02:45:30 +0200

    Fix url for gym views

--- website/__init__.py
+++ website/__init__.py
@@ -55,7 +55,7 @@ def create_app(test_config=None):
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/auth")
-    app.register_blueprint(gym, url_prefix="/auth")
+    app.register_blueprint(gym, url_prefix="/gym")
 
     from .models import db
 
