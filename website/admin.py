5486e14c9df9c6440b7925d96a93b1a9a3054845
Matth-M <matthieu.matsima@gmail.com>
2023-04-30 00:55:15 +0200

    Add views to the admin dashboard

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,25 @@
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+from .models import Reservation, User
+from .models import db
+
 
 admin = Admin()
+
+
+class UserView(ModelView):
+    column_exclude_list = [
+        "password",
+    ]
+
+
+class ReservationView(ModelView):
+    column_list = [
+        "time",
+        "user.username",
+    ]
+
+
+admin.add_view(UserView(User, db.session))
+admin.add_view(ReservationView(Reservation, db.session))
